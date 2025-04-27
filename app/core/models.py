e6dad092f654d9973aaf0f8076db4bcb2da33e56
Ndifoin Hilary <ndifoinhilary@gmail.com>
2025-04-27 13:54:00 +0100

    Define the Recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -38,3 +38,19 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+
+class Recipe(models.Model):
+    """
+    Custom recipe model
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    link = models.URLField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,8 +1,12 @@
 """
 Test for models
 """
+from decimal import Decimal
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+from core import models
 
 User = get_user_model()
 
@@ -27,3 +31,24 @@ class ModelTests(TestCase):
         """
         with self.assertRaises(ValueError):
             User.objects.create_user(email=None, password='test!2424f')
+
+
+
+    def test_create_recipe(self):
+        """
+        Test creating a new recipe is successful
+        :return:
+        """
+        user = User.objects.create_user(
+            email='test@example.com',
+            password='password'
+        )
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Test Recipe',
+            time_minutes=5,
+            price=Decimal('23.00'),
+            description='Test Description',
+        )
+        self.assertEqual( str(recipe),  recipe.title)
+        self.assertEqual(recipe.time_minutes, 5)
